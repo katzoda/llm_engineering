b19c0985af9531898eaa5c32da41c340e1c55a55
katzoda <daniel.katzor@gmail.com>
2025-11-29 17:42:57 +0100

    Added functions using Playwright.

--- 02_synthetiser-creator/scraper.py
+++ 02_synthetiser-creator/scraper.py
@@ -1,13 +1,8 @@
 from bs4 import BeautifulSoup
+import asyncio
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
-import requests
 
-
-# Standard headers to fetch a website
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-}
 
 async def scrape_site(url):
     # initialize Stealth with Playwright
@@ -30,34 +25,33 @@ async def scrape_site(url):
             irrelevant.decompose()
         text = soup.get_text(separator="\n", strip=True)
 
-        return (title + "\n\n" + text[:200])
+        return (title + "\n\n" + text[:2000])
 
-def fetch_website_contents(url):
-    """
-    Return the title and contents of the website at the given url
-    """
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    # retrieve the string from <title> element
-    title = soup.title.string if soup.title else "No title found"
-    if soup.body:
-        # delete not wanted html elements
-        for element in soup.body(["script", "style", "img", "input"]):
-            element.decompose()
-        # extract all visible text from the <body>
-        text = soup.body.get_text(separator="\n", strip=True)
-    else:
-        text = ""
-    return (title + "\n\n" + text)[:1_000]
+# This function scrapes one page, creates a scrape job for playwright function by scrape_multiple_sites(urls):
+async def fetch_site(page, url):
+    await page.goto(url, wait_until="networkidle")
+    title = await page.title()
+    text = await page.content()
+    return url, title, text
 
 
-def fetch_website_links(url):
-    """
-    Return the links on the webiste at the given url
-    I realize this is inefficient as we're parsing twice! This is to keep the code in the lab simple.
-    Feel free to use a class and optimize it!
-    """
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
-    links = [link.get("href") for link in soup.find_all("a")]
-    return [link for link in links if link]
+async def scrape_multiple_sites(urls):
+    # initialize Stealth with Playwright
+    async with Stealth().use_async(async_playwright()) as p:
+
+        # launch headless browser
+        browser = await p.chromium.launch(headless=True)
+
+        tasks = []
+
+        for url in urls:
+            page = await browser.new_page()
+            # schedule an async scraping job for each page
+            tasks.append(fetch_site(page, url))
+
+        # Run all scraping tasks concurrently
+        results = await asyncio.gather(*tasks)
+
+        await browser.close()
+     
+        return results
